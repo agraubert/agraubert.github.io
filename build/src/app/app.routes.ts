5d407ebc7a4fe68c0ddc59a278edcc4dec66edf8
Aaron Graubert <captianjroot@live.com>
2017-02-09 15:48:41 -0600

    Attempting to get the seed site to build

---
+++ build/src/app/app.routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+
+export const rootRouterConfig: Routes = [
+  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: 'home', component: HomeComponent },
+  // { path: 'about', component: AboutComponent },
+  // { path: 'github', component: RepoBrowserComponent,
+  //   children: [
+  //     { path: '', component: RepoListComponent },
+  //     { path: ':org', component: RepoListComponent,
+  //       children: [
+  //         { path: '', component: RepoDetailComponent },
+  //         { path: ':repo', component: RepoDetailComponent }
+  //       ]
+  //     }]
+  // },
+  // { path: 'contact', component: ContactComponent }
+];
