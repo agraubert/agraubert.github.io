fbc072fe1845f47ce550ee0365ac92f0b3ad5c50
Aaron Graubert <captianjroot@live.com>
2017-02-10 12:58:01 -0600

    Enabled JS minimization on deploy

--- build/src/app/app.component.ts
+++ build/src/app/app.component.ts
@@ -1,15 +1,10 @@
 import {Component} from '@angular/core';
-// import {Router} from '@angular/router';
-// import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 
 @Component({
   selector: 'app',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  // router: Router;
   loading: boolean = true;
-  // constructor(router: Router) {
-  //   this.router = router;
-  // }
+
 }
