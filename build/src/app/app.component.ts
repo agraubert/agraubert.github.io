66c4c769b50f5ccfdb256971643ecac011b93861
Aaron Graubert <captianjroot@live.com>
2017-02-10 17:06:46 -0600

    Switched to using router events to toggle the loading bar

--- build/src/app/app.component.ts
+++ build/src/app/app.component.ts
@@ -1,4 +1,12 @@
 import {Component} from '@angular/core';
+import {
+  Router,
+  Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router';
 
 @Component({
   selector: 'app',
@@ -6,4 +14,25 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   loading: boolean = true;
+
+  constructor(router: Router) {
+    router.events.subscribe((event: RouterEvent) => {
+      this.intercept(event);
+    })
+  }
+
+  intercept(event: RouterEvent) {
+    if (event instanceof NavigationStart) {
+      this.loading = true;
+    }
+    if (event instanceof NavigationEnd) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationCancel) {
+      this.loading = false;
+    }
+    if (event instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
 }
