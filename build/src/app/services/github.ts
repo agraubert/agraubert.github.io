0ea0cd0bca0ffbb4c777729bf0bb3ebf1d737821
Aaron Graubert <captianjroot@live.com>
2017-02-17 16:35:47 -0600

    Added caching for the repos list
    Tryin' to stay below the github api rate cap

--- build/src/app/services/github.ts
+++ build/src/app/services/github.ts
@@ -10,14 +10,13 @@ import {Observable} from 'rxjs/Observable';
 @Injectable()
 export class GithubService {
   private urls: Observable<any>;
+  private repos: Observable<any>;
 
   constructor(private http: Http) {
+    //having a fallback for the api rate would be really neat
     this.urls = this.makeRequest('https://api.github.com');
     this.urls.subscribe((response) => this.urls = Observable.of(response).last());
-  }
-
-  getRepos() {
-    return this.urls
+    this.repos = this.urls
       .map((response) => this.makeRequest(
         response.user_repositories_url
           .replace('{user}', 'agraubert')
@@ -25,6 +24,22 @@ export class GithubService {
       ))
       .switch();
   }
+
+  getRepos() {
+    this.repos.subscribe((response) => this.repos = Observable.of(response).last());
+    return this.repos;
+  }
+
+  // getMessage(repo: string) {
+  //   return this.urls
+  //     .map((response) => this.makeRequest(
+  //       response.repository_url
+  //         .replace('{owner}', 'agraubert')
+  //         .replace('{repo}', repo) + "?per_page=1"
+  //     ))
+  //     .switch()
+  //     .map((response) => response[0].message);
+  // }
 
   private makeRequest(url: string){
     console.log("FETCH: ", url);
