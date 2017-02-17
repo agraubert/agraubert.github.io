5c32a5061f325ae43a024307d16eaca897af7616
Aaron Graubert <captianjroot@live.com>
2017-02-17 12:46:57 -0600

    Improved github service with caching behavior
    After the constructor's request finishes, the inital observable is replaced with a constant observable

--- build/src/app/services/github.ts
+++ build/src/app/services/github.ts
@@ -2,32 +2,28 @@ import { Injectable} from '@angular/core';
 import { Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/last';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/exhaust';
+import 'rxjs/add/operator/switch';
+import 'rxjs/Rx';
 import {Observable} from 'rxjs/Observable';
 
 
 @Injectable()
 export class GithubService {
-  private user_url: string;
-  private repos_url: Observable<string>;
+  private urls: Observable<any>;
 
   constructor(private http: Http) {
-    this.repos_url = this.makeRequest('https://api.github.com')
-      .flatMap((response) => {
-        this.user_url = response.user_url.replace('{user}', 'agraubert');
-        return this.makeRequest(this.user_url)
-      })
-      .map((response) => response.repos_url);
-      // .subscribe(function(res) {
-      //   this.repos_url = res.repos_url;
-      // });
+    this.urls = this.makeRequest('https://api.github.com');
+    this.urls.subscribe((response) => this.urls = Observable.of(response).last());
   }
 
   getRepos() {
-    return this.repos_url
-      .map((response) => this.makeRequest(response))
-      .exhaust();
+    return this.urls
+      .map((response) => this.makeRequest(
+        response.user_repositories_url
+          .replace('{user}', 'agraubert')
+          .replace(/{\?.*?}/, '')
+      ))
+      .switch();
   }
 
   private makeRequest(url: string){
