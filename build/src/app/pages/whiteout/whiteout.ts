80cd150be8b5b6b4e6882e99ffdc4b699bbad620
Aaron Graubert <captianjroot@live.com>
2017-03-23 09:09:58 -0500

    Added whiteout link

--- build/src/app/pages/whiteout/whiteout.ts
+++ build/src/app/pages/whiteout/whiteout.ts
@@ -3,7 +3,8 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'whiteout',
   templateUrl: './whiteout.html',
+  styleUrls: ['./whiteout.css'],
 })
 export class WhiteoutComponent{
-  
+
 }
